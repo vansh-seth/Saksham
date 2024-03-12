2c45c8a7dcf2ac51f04723eae777ad96d374b2cf
Vansh Seth <111755254+vansh-seth@users.noreply.github.com>
2024-03-12 10:47:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
+import { useJsApiLoader } from '@react-google-maps/api';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { mapOptions } from './pages/MapConfigration';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Vision from './pages/Vision';
 import Home from './pages/Home';
 import Vision1 from './pages/Vision1';
 import Deaf from './pages/Deaf';
-import Map from './pages/Map';
 import Physical from './pages/Physical';
 import Mental from './pages/Mental';
 import Prosthetics from './pages/Prosthetics';
 import Alert from './pages/Alert';
+import Map from './pages/Map';
 function App() {
+
+  const { isLoaded } = useJsApiLoader({
+    id: mapOptions.googleMapApiKey,
+    googleMapsApiKey: mapOptions.googleMapApiKey,
+  });
+
+
   return (
     <BrowserRouter>
       <Routes>
@@ -22,7 +31,7 @@ function App() {
         <Route exact path="/Home" element={<Home />} />
         <Route exact path="/Vision1" element={<Vision1 />} />
         <Route exact path="/Deaf" element={<Deaf />} /> 
-        <Route exact path="/Map" element={<Map />} />
+        <Route exact path= "/map" element={<Map isLoaded={isLoaded}/>}  />
         <Route exact path="/Physical" element={<Physical />} />
         <Route exact path="/Mental" element={<Mental />} />
         <Route exact path="/Prosthetics" element={<Prosthetics />} />
